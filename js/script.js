4db4339fda3f99a4bcaa6d1acfb5d0e2f32f8431
pwachira900@gmail.com <pwachira900@gmail.com>
2019-02-17 20:49:34 +0300

    add BDD model

--- js/script.js
+++ js/script.js
@@ -5,22 +5,94 @@
   var b = parseInt(document.getElementById("b").value);
   var c = parseInt(document.getElementById("c").value);
 
-  var sides=[a,b,c];
+  var sides = [a,b,c];
 
   if (sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]){
-    alert("A triangle can not be formed from these sides.");
+
+     document.getElementById("answer").innerHTML = "A triangle can not be formed from these sides.";
   }
     else if (sides[0]===sides[1] && sides[1]===sides[2] && sides[0]===sides[2]){
-      alert("Thid is an equilateral triangle");
+
+      document.getElementById("answer").innerHTML = "This is an equilateral triangle. ";
     }
      else if (sides[0]===sides[1]||sides[0]===sides[2]||sides[1]===sides[2]) {
       // return "isosceles";
-      alert("This is an isoceles triangle");
+      document.getElementById("answer").innerHTML = "This is an isoceles triangle. ";
     }
     else if(sides[0]+sides[1]>sides[2]||sides[1]+sides[2]>sides[0]||sides[0]+sides[2]>sides[1]){
       // return "scalene";
-      alert("This is a scalene triangle");
+       document.getElementById("answer").innerHTML = "This is a scalene triangle. ";
     } else {
-    alert("invalid entry.");
+
+      document.getElementById("answer").innerHTML = "invalid entry.";
   }
-}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//  function triangle(){
+//   var a = parseInt(document.getElementById("a").value);
+//   var b = parseInt(document.getElementById("b").value);
+//   var c = parseInt(document.getElementById("c").value);
+//
+//   var sides=[a,b,c];
+//
+//   if (sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]){
+//     alert("A triangle can not be formed from these sides.");
+//   }
+//     else if (sides[0]===sides[1] && sides[1]===sides[2] && sides[0]===sides[2]){
+//       alert("Thid is an equilateral triangle");
+//     }
+//      else if (sides[0]===sides[1]||sides[0]===sides[2]||sides[1]===sides[2]) {
+//       // return "isosceles";
+//       alert("This is an isoceles triangle");
+//     }
+//     else if(sides[0]+sides[1]>sides[2]||sides[1]+sides[2]>sides[0]||sides[0]+sides[2]>sides[1]){
+//       // return "scalene";
+//       alert("This is a scalene triangle");
+//     } else {
+//     alert("invalid entry.");
+//   }
+// }
--- scritp2.js
+++ scritp2.js
@@ -4,25 +4,27 @@
   var a = parseInt(document.getElementById("a").value);
   var b = parseInt(document.getElementById("b").value);
   var c = parseInt(document.getElementById("c").value);
-  var result = [];
+  
   var sides = [a,b,c];
 
   if (sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]){
-     result = "A triangle can not be formed from these sides.";
+
+     document.getElementById("answer").innerHTML = "A triangle can not be formed from these sides.";
   }
     else if (sides[0]===sides[1] && sides[1]===sides[2] && sides[0]===sides[2]){
-      result = "Thid is an equilateral triangle";
+
+      document.getElementById("answer").innerHTML = "Thid is an equilateral triangle. ";
     }
      else if (sides[0]===sides[1]||sides[0]===sides[2]||sides[1]===sides[2]) {
       // return "isosceles";
-      result = "This is an isoceles triangle";
+      document.getElementById("answer").innerHTML = "This is an isoceles triangle. ";
     }
     else if(sides[0]+sides[1]>sides[2]||sides[1]+sides[2]>sides[0]||sides[0]+sides[2]>sides[1]){
       // return "scalene";
-      result = "This is a scalene triangle";
+       document.getElementById("answer").innerHTML = "This is a scalene triangle. ";
     } else {
-    result = "invalid entry.";
+
+      document.getElementById("answer").innerHTML = "invalid entry.";
   }
-   document.getElementById("answer").innerHTML = result;
 
 };
